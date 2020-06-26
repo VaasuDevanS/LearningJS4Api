638e5130d6b61573fcec29325e2c69423e1eb8bc
Vaasudevan <vaasuceg.96@gmail.com>
2020-06-25 21:31:24 -0300

    added initial files, map display

---
+++ README.md
@@ -0,0 +1,1 @@
+# LearningJS4Api
---
+++ functions.js
@@ -0,0 +1,9 @@
+/*
+Developer: Vaasudevan Srinivasan
+Created on: Jun 25, 2020
+Last Modified on: Jun 25, 2020
+Description: Script with useful Javascript functions
+*/
+
+
+console.log("Hello world..!!");
